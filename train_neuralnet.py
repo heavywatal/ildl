3cb831221f42d753c4014518236227311773fefa
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-12-23 23:40:43 +0900

    Set sys.path; clean code

--- train_neuralnet.py
+++ train_neuralnet.py
@@ -1,43 +1,45 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)
-
 import numpy as np
+import sys
+
+sys.path.append('deep-learning-from-scratch')
 from dataset.mnist import load_mnist
+
+sys.path.append('deep-learning-from-scratch/ch05')
 from two_layer_net import TwoLayerNet
 
-# データの読み込み
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True,
+                                                  one_hot_label=True)
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
-
+(train_size, input_size) = x_train.shape
+output_size = t_train.shape[1]
 iters_num = 10000
-train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
+iter_per_epoch = max(train_size / batch_size, 1)
+
+network = TwoLayerNet(input_size=input_size,
+                      hidden_size=50,
+                      output_size=output_size)
 
 train_loss_list = []
 train_acc_list = []
 test_acc_list = []
 
-iter_per_epoch = max(train_size / batch_size, 1)
-
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
     t_batch = t_train[batch_mask]
-    
-    # 勾配
+
     #grad = network.numerical_gradient(x_batch, t_batch)
     grad = network.gradient(x_batch, t_batch)
-    
-    # 更新
-    for key in ('W1', 'b1', 'W2', 'b2'):
+
+    for key in network.params.keys():
         network.params[key] -= learning_rate * grad[key]
-    
+
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-    
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
